e8ede8ef522d925abc8d337095f956af2bd71d20
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-09-16 21:50:38 +0800

    add v2.0

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -35,6 +35,7 @@ export const authOptions: NextAuthOptions = {
     TwitterProvider({
       clientId: env.TWITTER_CLIENT_ID,
       clientSecret: env.TWITTER_CLIENT_SECRET,
+      version: "2.0"
     }),
   ],
   secret: env.NEXTAUTH_SECRET,
