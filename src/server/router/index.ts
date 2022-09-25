4972533cc947c9f74ff7d55c88e666030ab78ecf
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-09-26 00:06:30 +0800

    cleanups

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,14 +2,12 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { protectedExampleRouter } from "./protected-example-router";
 import { coursesRouter } from "./routes/courses";
 import { lessonsRouter } from "./routes/lessons";
 import { schedulesRouter } from "./routes/schedules";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("auth.", protectedExampleRouter)
   .merge("courses.", coursesRouter)
   .merge("lessons.", lessonsRouter)
   .merge("schedules.", schedulesRouter);
